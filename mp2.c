6180e178f971be99a6433b16c23c30a20a7d689f
miguelgarcia94 <114983934+miguelgarcia94@users.noreply.github.com>
2022-10-04 10:54:10 +0200

    Update mp2.c

--- mp2.c
+++ mp2.c
@@ -5,22 +5,24 @@
 #include <sys/wait.h>
 
 void main() {
-  pid_t pid, pid_hijo;
+  pid_t pid, pid_hijo,pid_nieto;
   pid=fork();
-  printf("Soy el proceso p1")
+  
   if (pid==0){
   fork();
   	printf("Soy el proceso p2, el pid del hijo %d\n", getpid());//pid del proceso hijo
   	printf("el pid del padre %d\n", getppid());//pid del proceso padre
-  	prinff("")
+  	
   	
   	
   }
  	else{
- 	 fork();
+ 	wait(NULL); 	 
+ 	fork();
+ 	 
   	printf("Soy el proceso p3,el pid del hijo %d\n", getpid());//pid del proceso hijo
   	printf("el pid del padre %d\n", getppid());//pid del proceso padre
-  	prinff("")
+  	
   	}
 	exit(0);
   
